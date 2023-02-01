3ec67897ec4705f53b16bf8fc5a1d52b75eef037
Jukiyoomi <jnguessan2003@gmail.com>
2023-02-01 15:17:01 +0100

    FEAT: authenticate user through google

--- app/src/main/java/com/frite/creativevues/AuthActivity.java
+++ app/src/main/java/com/frite/creativevues/AuthActivity.java
@@ -1,18 +1,26 @@
 package com.frite.creativevues;
 
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -26,9 +34,9 @@ public class AuthActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        signInBtn = (SignInButton) findViewById(R.id.auth_btn);
+        signInBtn = findViewById(R.id.auth_btn);
 
-        GoogleSignInOptions googleSignInOptions=new GoogleSignInOptions.Builder(
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(
                 GoogleSignInOptions.DEFAULT_SIGN_IN
         ).requestIdToken("679842903143-tgkb52uard6oge3n9qfilfimpvgl9qak.apps.googleusercontent.com")
                 .requestEmail()
@@ -36,24 +44,64 @@ public class AuthActivity extends AppCompatActivity {
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
 
-        signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-                startActivityForResult(signInIntent, 100);
-            }
+        signInBtn.setOnClickListener(view -> {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            activityResultLauncher.launch(signInIntent);
         });
 
         mAuth = FirebaseAuth.getInstance();
-        // Initialize firebase user
-        FirebaseUser firebaseUser = mAuth.getCurrentUser();
-        // Check condition
-        if(firebaseUser != null)
-        {
-            // startActivity(new Intent(AuthActivity.this,ProfileActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-            Toast.makeText(this, "User already logged in", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "User not logged in", Toast.LENGTH_SHORT).show();
-        }
+    }
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    // There are no request codes
+                    Intent data = result.getData();
+
+                    // Initialize task
+                    Task<GoogleSignInAccount> signInAccountTask = GoogleSignIn
+                            .getSignedInAccountFromIntent(data);
+
+                    if(signInAccountTask.isSuccessful())
+                    {
+                        Toast.makeText(AuthActivity.this, "Processing authentication", Toast.LENGTH_SHORT).show();
+
+                        try {
+                            // Initialize sign in account
+                            GoogleSignInAccount googleSignInAccount = signInAccountTask
+                                    .getResult(ApiException.class);
+
+                            if(googleSignInAccount != null) {
+                                // Initialize auth credential
+                                AuthCredential authCredential = GoogleAuthProvider
+                                        .getCredential(googleSignInAccount.getIdToken(),null);
+
+                                // Check credential
+                                checkCredential(authCredential);
+                            }
+                        }
+                        catch (ApiException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Log.d(TAG, "onActivityResult: " + signInAccountTask.getException());
+                        Toast.makeText(AuthActivity.this, "Google sign in failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+    private void checkCredential(AuthCredential authCredential) {
+        mAuth.signInWithCredential(authCredential)
+                .addOnCompleteListener(this, task -> {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(AuthActivity.this, "Firebase authentication successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(AuthActivity.this, ProfileActivity.class)
+                                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                    } else {
+                        Toast.makeText(AuthActivity.this, "Firebase authentication failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
