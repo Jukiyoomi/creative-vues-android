7c2ef796b86c17af4b78d855ba03576874dbbb6d
Jukiyoomi <jnguessan2003@gmail.com>
2023-02-05 13:33:21 +0100

    FEAT: logout feature

--- app/src/main/java/com/frite/creativevues/ProfileActivity.java
+++ app/src/main/java/com/frite/creativevues/ProfileActivity.java
@@ -3,18 +3,27 @@ package com.frite.creativevues;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.frite.creativevues.db.CustomFirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
-    TextView title;
+    private static final String TAG = "ProfileActivity";
 
+    private Button logoutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        title = findViewById(R.id.profile_title);
+
+        logoutButton = (Button) findViewById(R.id.logout_btn);
+        logoutButton.setOnClickListener(v -> {
+            CustomFirebaseAuth.getInstance().logout();
+            Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(ProfileActivity.this, MainActivity.class);
+            startActivity(i);
+        });
 
         CustomFirebaseAuth firebaseUser = CustomFirebaseAuth.getInstance();
 
