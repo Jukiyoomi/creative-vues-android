1325ca99898b4b35f2412b9699202e66ef2123b0
Jukiyoomi <jnguessan2003@gmail.com>
2023-02-07 12:53:25 +0100

    FEAT: use firestore to save post

--- app/src/main/java/com/frite/creativevues/NewPostActivity.java
+++ app/src/main/java/com/frite/creativevues/NewPostActivity.java
@@ -2,12 +2,22 @@ package com.frite.creativevues;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.frite.creativevues.db.CustomFirebaseAuth;
+import com.frite.creativevues.db.DBProvider;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class NewPostActivity extends AppCompatActivity {
     private static final String TAG = "NewPostActivity";
@@ -15,7 +25,6 @@ public class NewPostActivity extends AppCompatActivity {
     private TextView counter;
     private EditText postText;
     private Button submitBtn;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +45,7 @@ public class NewPostActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 setCounter(s.length());
 
-                if (s.length() > MAX_POST_LENGTH) {
+                if (isPostTooLong()) {
                     counter.setTextColor(getResources().getColor(R.color.red, null));
                 } else {
                     counter.setTextColor(getResources().getColor(R.color.blue, null));
@@ -46,9 +55,51 @@ public class NewPostActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {}
         });
+
+        submitBtn.setOnClickListener(v -> {
+            if (isPostEmpty() || isPostTooLong()) {
+                postText.setError("Post can neither be empty nor longer than " + MAX_POST_LENGTH + " characters");
+            } else {
+                HashMap<String, Object> data = new HashMap<>();
+
+                data.put("text", postText.getText().toString().trim());
+                data.put("user", CustomFirebaseAuth.getInstance().getUser().getUid());
+                data.put("avatar", Objects.requireNonNull(CustomFirebaseAuth.getInstance().getUser().getPhotoUrl()).toString());
+                data.put("username", CustomFirebaseAuth.getInstance().getUser().getDisplayName());
+                data.put("modified", false);
+                data.put("likes", new ArrayList<>());
+                data.put("timestamp", DBProvider.getInstance().getTimestamp());
+
+                try {
+                    DBProvider.getInstance().getDb()
+                            .collection("posts")
+                            .add(data)
+                            .addOnSuccessListener(documentReference -> {
+                                Log.d(TAG, "Post written successfully !");
+                                Toast.makeText(NewPostActivity.this, "Post written successfully !", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(NewPostActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            })
+                            .addOnFailureListener(e -> {
+                                Log.d(TAG, "Error adding document", e);
+                                Toast.makeText(NewPostActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private void setCounter(int count) {
         counter.setText(count + "/" + MAX_POST_LENGTH);
+    }
+
+    private boolean isPostTooLong() {
+        return postText.getText().length() >= MAX_POST_LENGTH;
+    }
+
+    private boolean isPostEmpty() {
+        return postText.getText().length() == 0;
     }
 }
